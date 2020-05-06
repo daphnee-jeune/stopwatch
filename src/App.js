3f516758a1988549275b366515c84a276e3e9f14
Daphnee <daphneejeune.dev@gmail.com>
2020-05-06 19:12:05 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import Display from './components/Display'
+import Btn from './components/Btn'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [time, setTime] = useState({
+      ms:0,
+      s:0, 
+      m:0, 
+      h:0
+    })
+  const [interv, setInterv] = useState()
+  const [status, setStatus] = useState(0)
+  // Not started = 0
+  // started = 1
+  // stopped = 2
+
+  const start = () => {
+    run()
+    setStatus(1)
+    setInterv(setInterval(run, 10))
+  }
+
+  let mostRecentMs = time.ms, mostRecentS = time.s, mostRecentM = time.m, mostRecentH = time.h
+
+  const run = () => {
+    if(mostRecentM === 60){
+      mostRecentH++
+      mostRecentM = 0
+    }
+    if(mostRecentS === 60){
+      mostRecentM++
+      mostRecentS = 0
+    }
+    if(mostRecentMs === 100){
+      mostRecentS++
+      mostRecentMs = 0
+    }
+    mostRecentMs++
+    return setTime({ms:mostRecentMs, s:mostRecentS, m:mostRecentM, h:mostRecentH})
+  }
+
+  const stop = () => {
+    clearInterval(interv)
+    setStatus(2)
+  }
+
+  const reset = () => {
+    clearInterval(interv)
+    setStatus(0)
+    setTime({ms:0, s:0, m:0, h:0})
+  }
+
+  const resume = () => start()
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <div className="clock">
+          <div className="stopwatch">
+               <Display time={time}/>
+               <Btn status={status} resume={resume} reset={reset} stop={stop} start={start}/>
+          </div>
+     </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Btn.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Btn = ({status, start, stop, reset, resume}) => {
+  return (
+    <div>
+      {(status === 0)? 
+        <button className="stopwatch-btn stopwatch-btn-gre"
+        onClick={start}>Start</button> : ""
+      }
+
+      {(status === 1)? 
+        <div>
+          <button className="stopwatch-btn stopwatch-btn-red"
+                  onClick={stop}>Stop</button>
+          <button className="stopwatch-btn stopwatch-btn-yel"
+                  onClick={reset}>Reset</button>
+        </div> : ""
+      }
+
+     {(status === 2)? 
+        <div>
+          <button className="stopwatch-btn stopwatch-btn-gre"
+                  onClick={resume}>Resume</button>
+          <button className="stopwatch-btn stopwatch-btn-yel"
+                  onClick={reset}>Reset</button>
+        </div> : ""
+      }
+     
+    </div>
+  );
+}
+
+export default Btn
